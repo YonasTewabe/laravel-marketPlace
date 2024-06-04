bfaf453080834df818013872a1e98b86db7bd86d
YonasTewabe <yonastewabe21@gmail.com>
2024-06-04 06:11:48 -0500

    setting up database

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,34 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return  [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'lorem    epsum',
-                'price' => '200'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'something',
-                'price' => '200'
-            ]
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
